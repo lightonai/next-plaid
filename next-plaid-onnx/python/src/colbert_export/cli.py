155904b1303a9962b6c19ca04d40dee5a39fea04
Raphael Sourty <raphael.sourty@gmail.com>
2026-05-29 01:47:50 +0200

    chore: bump version to 1.5.0

--- next-plaid-onnx/python/src/colbert_export/cli.py
+++ next-plaid-onnx/python/src/colbert_export/cli.py
@@ -82,7 +82,7 @@ Supported models:
     parser.add_argument(
         "--version",
         action="version",
-        version="%(prog)s 1.4.0",
+        version="%(prog)s 1.5.0",
     )
 
     args = parser.parse_args()
